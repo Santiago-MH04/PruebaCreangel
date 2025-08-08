1dc8383bcc9e4f60b729a552ed9285ae7c5f3d06
Santiago <santimh04@gmail.com>
2025-08-07 20:04:09 -0500

    feat: CRUD completo en IndexCrawling

--- PruebaTecnica_SantiagoMarinHiguita/src/POO/CrawlingIndex.java
+++ PruebaTecnica_SantiagoMarinHiguita/src/POO/CrawlingIndex.java
@@ -1,9 +1,98 @@
 package POO;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import POO.modelos.Documento;
+import POO.repositorios.implementaciones.FuenteArchivo;
+import POO.repositorios.implementaciones.FuenteBaseDatos;
+import POO.repositorios.implementaciones.FuenteWeb;
+import POO.servicios.abstracciones.DocumentoServicio;
+import POO.servicios.implementaciones.FuenteArchivoServicioImpl;
+import POO.servicios.implementaciones.FuenteBaseDatosServicioImpl;
+import POO.servicios.implementaciones.FuenteWebServicioImpl;
+
+import java.util.List;
+import java.util.Optional;
+
 public class CrawlingIndex {
     public static void main(String[] args) {
+        //Iniciar las instancias fuente de cada tipo de archivos
+        FuenteArchivo fuenteArchivo = new FuenteArchivo();
+        FuenteBaseDatos fuenteBaseDatos = new FuenteBaseDatos();
+        FuenteWeb fuenteWeb = new FuenteWeb();
+
+        //Iniciar los servicios
+        DocumentoServicio fuenteArchivoServicio = new FuenteArchivoServicioImpl(fuenteArchivo);
+        DocumentoServicio fuenteBaseDatosServicio = new FuenteBaseDatosServicioImpl(fuenteBaseDatos);
+        DocumentoServicio fuenteWebServicio = new FuenteWebServicioImpl(fuenteWeb);
+
+
+        //Listar los distintos documentos
+        List<Documento> documentosFuenteArchivo = fuenteArchivoServicio.obtenerDocumentos();
+        List<Documento> documentosFuenteBaseDatos = fuenteBaseDatosServicio.obtenerDocumentos();
+        List<Documento> documentosFuenteWeb = fuenteWebServicio.obtenerDocumentos();
+
+        System.out.println("Imprimiendo los distintos documentos");
+        System.out.println("Fuente archivo");
+        documentosFuenteArchivo.forEach(System.out::println);
+        System.out.println("\nFuente base de datos");
+        documentosFuenteBaseDatos.forEach(System.out::println);
+        System.out.println("\nFuente web");
+        documentosFuenteWeb.forEach(System.out::println);
+
+        System.out.println("\nBuscando un elemento de cada servicio");
+        Documento documentoFuenteArchivo = fuenteArchivoServicio.obtenerDocumentoPorTitulo("documento2").get();
+        Documento documentoFuenteWeb = fuenteWebServicio.obtenerDocumentoPorTitulo("documento4").get();
+        Documento documentoFuenteBaseDatos = fuenteBaseDatosServicio.obtenerDocumentoPorTitulo("documento9").get();
+
+        System.out.println(documentoFuenteArchivo);
+        System.out.println(documentoFuenteBaseDatos);
+        System.out.println(documentoFuenteWeb);
+
+        System.out.println("\nAgregando un elemento a cada servicio");
+        Documento nuevoFuenteArchivo = new Documento("Nuevo doc", "Nuevo documento de prueba", "FuenteArchivo");
+        Documento nuevoFuenteBaseDatos = new Documento("Nueva B.D.", "Otra base de datos sin información relevante", "FuenteBaseDatos");
+        Documento nuevoFuenteWeb = new Documento("Otra landing page", "Una landing page de otro emprendimiento", "FuenteWeb");
+
+        fuenteArchivoServicio.agregarDocumento(nuevoFuenteArchivo);
+        fuenteBaseDatosServicio.agregarDocumento(nuevoFuenteBaseDatos);
+        fuenteWebServicio.agregarDocumento(nuevoFuenteWeb);
+
+        System.out.println("Imprimiendo los distintos documentos");
+        System.out.println("Fuente archivo");
+        fuenteArchivoServicio.obtenerDocumentos().forEach(System.out::println);
+        System.out.println("\nFuente base de datos");
+        fuenteBaseDatosServicio.obtenerDocumentos().forEach(System.out::println);
+        System.out.println("\nFuente web");
+        fuenteWebServicio.obtenerDocumentos().forEach(System.out::println);
+
+        System.out.println("\nModificando un elemento en cada servicio");
+        nuevoFuenteArchivo.setContenido("Modificando ando este registro");
+        nuevoFuenteBaseDatos.setTitulo("Nueva vieja B.D.");
+        nuevoFuenteWeb.setContenido("Una nueva versión de la misma landing page");
+
+        fuenteArchivoServicio.actualizarDocumento("Nuevo doc", nuevoFuenteArchivo);
+        fuenteBaseDatosServicio.actualizarDocumento("Nueva B.D.", nuevoFuenteBaseDatos);
+        fuenteWebServicio.actualizarDocumento("Otra landing page", nuevoFuenteWeb);
+
+        System.out.println("Imprimiendo los distintos documentos");
+        System.out.println("Fuente archivo");
+        fuenteArchivoServicio.obtenerDocumentos().forEach(System.out::println);
+        System.out.println("\nFuente base de datos");
+        fuenteBaseDatosServicio.obtenerDocumentos().forEach(System.out::println);
+        System.out.println("\nFuente web");
+        fuenteWebServicio.obtenerDocumentos().forEach(System.out::println);
+
+        System.out.println("\nEliminando un elemento de cada servicio");
+        fuenteArchivoServicio.eliminarDocumentoPorTitulo(nuevoFuenteArchivo.getTitulo());
+        fuenteBaseDatosServicio.eliminarDocumentoPorTitulo(nuevoFuenteBaseDatos.getTitulo());
+        fuenteWebServicio.eliminarDocumentoPorTitulo(nuevoFuenteWeb.getTitulo());
+
+        System.out.println("Imprimiendo los distintos documentos");
+        System.out.println("Fuente archivo");
+        fuenteArchivoServicio.obtenerDocumentos().forEach(System.out::println);
+        System.out.println("\nFuente base de datos");
+        fuenteBaseDatosServicio.obtenerDocumentos().forEach(System.out::println);
+        System.out.println("\nFuente web");
+        fuenteWebServicio.obtenerDocumentos().forEach(System.out::println);
 
     }
 }
--- PruebaTecnica_SantiagoMarinHiguita/src/POO/repositorios/implementaciones/FuenteDocumento.java
+++ PruebaTecnica_SantiagoMarinHiguita/src/POO/repositorios/implementaciones/FuenteDocumento.java
@@ -3,27 +3,31 @@ package POO.repositorios.implementaciones;
 import POO.modelos.Documento;
 import POO.repositorios.abstracciones.FuenteDatos;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.IntStream;
 
 public abstract class FuenteDocumento implements FuenteDatos {
+    private static List<Documento> documentosList = new ArrayList<>(Arrays.asList(
+        new Documento("Documento1", "Historia de un día que ocurrió jamás", "FuenteArchivo"),
+        new Documento("Documento2", "Registro de divorcios de parejas que nunca se casaron", "FuenteArchivo"),
+        new Documento("Documento3", "Conteo de goles al arcoíris Mundial 1990&Nunca", "FuenteArchivo"),
+
+        new Documento("Documento4", "Página web de un banco que no roba", "FuenteWeb"),
+        new Documento("Documento5", "Recorrido web por la ciudad invisible", "FuenteWeb"),
+        new Documento("Documento6", "Cirugía virtual para volver más feos a los feos", "FuenteWeb"),
+
+        new Documento("Documento7", "Base de datos de gente que ha visto volar a los cerdos", "FuenteBaseDatos"),
+        new Documento("Documento8", "Base de datos de benefactores de Rico McPato", "FuenteBaseDatos"),
+        new Documento("Documento9", "Base de datos de personas que convierten el agua en vino", "FuenteBaseDatos")
+    ));
+
     @Override
     public List<Documento> obtenerDocumentos() {
-        return List.of(
-            new Documento("Documento1", "Historia de un día que ocurrió jamás", "FuenteArchivo"),
-            new Documento("Documento2", "Registro de divorcios de parejas que nunca se casaron", "FuenteArchivo"),
-            new Documento("Documento3", "Conteo de goles al arcoíris Mundial 1990&Nunca", "FuenteArchivo"),
-
-            new Documento("Documento4", "Página web de un banco que no roba", "FuenteWeb"),
-            new Documento("Documento5", "Recorrido web por la ciudad invisible", "FuenteWeb"),
-            new Documento("Documento6", "Cirugía virtual para volver más feos a los feo", "FuenteWeb"),
-
-            new Documento("Documento7", "Base de datos de gente que ha visto volar a los cerdos", "FuenteBaseDatos"),
-            new Documento("Documento8", "Base de datos de benefactores de Rico McPato", "FuenteBaseDatos"),
-            new Documento("Documento9", "Base de datos de personas que convierten el agua en vino", "FuenteBaseDatos")
-        );
+        return documentosList;
     }
 
     @Override
@@ -35,18 +39,18 @@ public abstract class FuenteDocumento implements FuenteDatos {
 
     @Override
     public void agregarDocumento(Documento documento) {
-        this.obtenerDocumentos().add(documento);
+        documentosList.add(documento);
     }
 
     @Override
     public boolean actualizarDocumento(String titulo, Documento documento) {
         AtomicBoolean actualizado = new AtomicBoolean(false);
 
-        IntStream.range(0, this.obtenerDocumentos().size())
-            .filter(i -> this.obtenerDocumentos().get(i).getTitulo().equalsIgnoreCase(titulo))
+        IntStream.range(0, documentosList.size())
+            .filter(i -> documentosList.get(i).getTitulo().equalsIgnoreCase(titulo))
             .findFirst()
             .ifPresent(i -> {
-                this.obtenerDocumentos().set(i, documento);
+                documentosList.set(i, documento);
                 actualizado.set(true);
             });
 
@@ -56,10 +60,11 @@ public abstract class FuenteDocumento implements FuenteDatos {
     @Override
     public void eliminarDocumentoPorTitulo(String titulo) {
         Optional<Documento> archivoAEliminar = this.obtenerDocumentoPorTitulo(titulo);
-        archivoAEliminar.ifPresent(a -> {
-            this.obtenerDocumentos().remove(a);
-            System.out.printf("Documento %s eliminado con éxito", a.getTitulo());
-        });
-        System.out.printf("El documento %s no se encuentra indexado", titulo);
+        if(archivoAEliminar.isPresent()){
+            documentosList.remove(archivoAEliminar);
+            System.out.printf("Documento %s eliminado con éxito\n", archivoAEliminar.get().getTitulo());
+            return;
+        }
+        System.out.printf("El documento %s no se encuentra indexado\n", titulo);
     }
 }
