00280a793ae06801e370bc2fe5c7ac8739d3f3c8
Santiago <santimh04@gmail.com>
2025-08-07 21:23:55 -0500

    feat: ejercicio de los palíndromos completo

---
+++ PruebaTecnica_SantiagoMarinHiguita/src/AlgoritmosYEstructurasDeDatos/PalindromoEvaluador.java
@@ -0,0 +1,55 @@
+package AlgoritmosYEstructurasDeDatos;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class PalindromoEvaluador {
+    public static String palindromoBuilder(String param) {
+        param = param.toLowerCase();
+        param = param.replaceAll(" ", "");
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        // Contar frecuencia de cada carácter
+        for (char c : param.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        int cuentaImpares = 0;
+        Character charMediana = null;
+        StringBuilder mediana = new StringBuilder();
+
+        // Verificar posibilidad y construir mitad del palíndromo
+        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+            char ch = entry.getKey();
+            int frecuencia = entry.getValue();
+
+            if (frecuencia % 2 != 0) {
+                cuentaImpares++;
+                if (cuentaImpares > 1) return "Not Possible";
+                charMediana = ch;
+            }
+
+            // Agregar la primera mitad de los caracteres
+            for (int i = 0; i < frecuencia / 2; i++) {
+                mediana.append(ch);
+            }
+        }
+
+        // Agregar la segunda mitad de los caracteres y construir palíndromo completo
+        StringBuilder palindrome = new StringBuilder(mediana);
+        if (charMediana != null) palindrome.append(charMediana);
+        palindrome.append(new StringBuilder(mediana).reverse());
+
+        return palindrome.toString();
+    }
+
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Ingresa una línea con caracteres alfanuméricos: ");
+            String stringEntrada = sc.nextLine();
+            System.out.println(palindromoBuilder(stringEntrada));
+        }
+    }
+}
